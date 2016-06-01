b98edb3f3e60fbe380485cd777fbcd8f8e4a0c11
Andrew Pawloski <Andrew@element84.com>
2016-06-01 10:38:58 -0400

    Added proxy to npm calls

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -12,12 +12,16 @@ install_nodejs() {
   local dir="$2"
 
   if needs_resolution "$version"; then
-    echo "Resolving node version ${version:-(latest stable)} via semver.io..."
-    local version=$(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/node/resolve)
+      # Keep a list of supported versions here and compare
+      echo "Only 4.0.0 supported as of now"
+      
+    # echo "Resolving node version ${version:-(latest stable)} via semver.io..."
+    # local version=$(curl --silent --get --retry 5 --retry-max-time 15 --data-urlencode "range=${version}" https://semver.herokuapp.com/node/resolve)
   fi
 
   echo "Downloading and installing node $version..."
-  local download_url="https://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
+  #  local download_url="https://s3pository.heroku.com/node/v$version/node-v$version-$os-$cpu.tar.gz"
+  local download_url="https://nodejs.org/dist/v$version/node-v$version-linux-x64.tar.gz"
   curl "$download_url" --silent --fail  --retry 5 --retry-max-time 15 -o /tmp/node.tar.gz || (echo "Unable to download node $version; does it exist?" && false)
   tar xzf /tmp/node.tar.gz -C /tmp
   rm -rf $dir/*
--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -1,9 +1,19 @@
+# GP-MCE fix
+set_local_config() {
+    npm config set registry "http://registry.npmjs.org/"
+    npm config set proxy "$http_proxy"
+    npm config set https-proxy "$https_proxy"
+    npm set strict-ssl false
+    npm config list
+}
+
 run_if_present() {
   local script_name=${1:-}
   local has_script=$(read_json "$BUILD_DIR/package.json" ".scripts[\"$script_name\"]")
   if [ -n "$has_script" ]; then
-    echo "Running $script_name"
-    npm run "$script_name" --if-present
+      echo "Running $script_name"
+      set_local_config
+      npm run "$script_name" --if-present
   fi
 }
 
@@ -18,7 +28,8 @@ install_node_modules() {
     else
       echo "Installing node modules (package.json)"
     fi
-    npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
+    set_local_config
+    npm install --unsafe-perm 2>&1 # --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
   fi
@@ -36,6 +47,7 @@ rebuild_node_modules() {
     else
       echo "Installing any new modules (package.json)"
     fi
+    set_local_config
     npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
